9d6ad8060ade4e13d9a20fe04b776a75bbe34baf
Ivanovich Chiu <2bavilaoiac@gmail.com>
2025-05-12 23:28:54 -0600

    Primer commit

---
+++ Hackathon2025/Hackathon2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon2025App.swift
+//  Hackathon2025
+//
+//  Created by Ivanovich Chiu on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hackathon2025Tests/Hackathon2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon2025Tests.swift
+//  Hackathon2025Tests
+//
+//  Created by Ivanovich Chiu on 12/05/25.
+//
+
+import Testing
+@testable import Hackathon2025
+
+struct Hackathon2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
